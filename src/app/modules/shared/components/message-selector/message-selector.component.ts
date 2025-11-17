cdaf8772b5cb182c3e85ee9aaee50816975c97d0
faezehFT <penelopedandelion96ft@gmail.com>
2025-11-18 01:08:58 +0330

    s

--- src/app/modules/shared/components/message-selector/message-selector.component.ts
+++ src/app/modules/shared/components/message-selector/message-selector.component.ts
@@ -4,74 +4,81 @@ import {NeobankService} from "../../../main/services/neobank.service";
 
 import { TranslocoService } from '@ngneat/transloco';
 import { FuseLoadingService } from '../../../../../@fuse/services/loading';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-message-selector',
     templateUrl: './message-selector.component.html',
     standalone: true,
     styleUrls: ['./message-selector.component.scss'],
+    imports: [DropdownModule, FormsModule],
 })
 export class MessageSelectorComponent implements OnInit {
-    @Input() inputMessage
-    statusCodeOptions = ApiGatewayConstants.statusCode
-    codeMessage
-    titleMessage
-    textMessage
-    textENMessage
-    typeMessage
-    tableIdMessage
-    messageId
-    selectedMessageId
-    icon_val
-    tableIdDe
-    titleMessageDe
-    rows
-    messageIdDe
-    codeMessageDe
-    first
-    messageFlag:boolean=false
-    messagesList = []
+    @Input() inputMessage;
+    statusCodeOptions = ApiGatewayConstants.statusCode;
+    codeMessage;
+    titleMessage;
+    textMessage;
+    textENMessage;
+    typeMessage;
+    tableIdMessage;
+    messageId;
+    selectedMessageId;
+    icon_val;
+    tableIdDe;
+    titleMessageDe;
+    rows;
+    messageIdDe;
+    codeMessageDe;
+    first;
+    messageFlag: boolean = false;
+    messagesList = [];
     typeMessages = ApiGatewayConstants.typeMessages;
     categoryMessages = ApiGatewayConstants.categoryMessages;
-    paginationLabel=this.transloco.translate('label.pagination.table');
-  constructor(
-      private messagesApiFacadeService:NeobankService,
-      private transloco :TranslocoService,
-      private _primengProgressBarService: FuseLoadingService,
-  ) { }
+    paginationLabel = this.transloco.translate('label.pagination.table');
+    constructor(
+        private messagesApiFacadeService: NeobankService,
+        private transloco: TranslocoService,
+        private _primengProgressBarService: FuseLoadingService
+    ) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
     messageSearch() {
-        this.messagesApiFacadeService.messagesearch(
-            this.codeMessage, this.titleMessage, this.tableIdMessage, this.typeMessage
-        ).subscribe(response => {
-            if (Array.isArray(response)) {
-                this.messagesList = response
-            } else {
-                this.messagesList.push(response)
-            }
-        })
+        this.messagesApiFacadeService
+            .messagesearch(
+                this.codeMessage,
+                this.titleMessage,
+                this.tableIdMessage,
+                this.typeMessage
+            )
+            .subscribe((response) => {
+                if (Array.isArray(response)) {
+                    this.messagesList = response;
+                } else {
+                    this.messagesList.push(response);
+                }
+            });
     }
     messageClear() {
-        this.titleMessage = ""
-        this.textMessage = ""
-        this.textENMessage= ""
-        this.tableIdMessage = ""
-        this.typeMessage= ""
-        this.messageId = null
-        this.messageSearch()
+        this.titleMessage = '';
+        this.textMessage = '';
+        this.textENMessage = '';
+        this.tableIdMessage = '';
+        this.typeMessage = '';
+        this.messageId = null;
+        this.messageSearch();
     }
     selectedMessage(event) {
-        this.messageId= event.data.messageId
-        this.selectedMessageId = event.data.messageId
-        this.codeMessageDe = event.data.code
-        this.messageIdDe = event.data.messageId
-        this.titleMessageDe = event.data.title
-        this.tableIdDe = event.data.tableId
-        this.icon_val = "pi pi-check"
+        this.messageId = event.data.messageId;
+        this.selectedMessageId = event.data.messageId;
+        this.codeMessageDe = event.data.code;
+        this.messageIdDe = event.data.messageId;
+        this.titleMessageDe = event.data.title;
+        this.tableIdDe = event.data.tableId;
+        this.icon_val = 'pi pi-check';
     }
     onRowUnselect(event) {
-        this.icon_val = null
+        this.icon_val = null;
     }
 }
